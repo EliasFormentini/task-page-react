d0d0bba2414759cdd3de72975c01969365c166c2
Elias <eliasformentini1@gmail.com>
2024-10-26 11:10:38 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import AddTarefa from "./componentes/AddTarefa";
+import Tarefas from "./componentes/Tarefas";
+import Title from "./componentes/Title";
+
+function App() {
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      //CHAMAR A API
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10",
+        {
+          method: "GET",
+        }
+      );
+
+      const data = await response.json();
+
+      setTasks(data);
+      console.log(data)
+    };
+
+    fetchTasks();
+  }, []);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      //PRECISO ATUALIZAR ESSA TAREFA
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      //NÃO PRECISO ATUALIZAR ESSA TAREFA
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: tasks.length + 1,
+      title,
+      description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
+        <Title>Gerenciador de Tarefas</Title>
+        <AddTarefa onAddTaskSubmit={onAddTaskSubmit} />
+        <Tarefas
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Title.jsx
@@ -0,0 +1,7 @@
+function Title(props) {
+  return (
+    <h1 className="text-4xl text-slate-100 font-bold">{ props.children }</h1>
+  );
+}
+
+export default Title;
